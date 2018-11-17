db521078b34c933cefbbf9db8858d41e9ac503b9
Elly <ellyhahn@gmail.com>
2018-11-16 20:02:18 -0800

    Add cart function

--- pages/product/productQueries.js
+++ pages/product/productQueries.js
@@ -1,12 +1,17 @@
 import { gql } from "apollo-boost";
-import { PRODUCT_FRAGMENT } from "../../fragment";
+import { PRODUCT_FRAGMENT } from "../../fragments";
 
 export const PRODUCT_QUERY = gql`
   query productQuery($id: ID!) {
     product(where: { id: $id }) {
       ...ProductItems
-      description
     }
   }
   ${PRODUCT_FRAGMENT}
+`;
+
+export const TOGGLE_CART = gql`
+  mutation toggleCart($id: ID!) {
+      toggleProduct(id: $id) @client
+  }
 `;
